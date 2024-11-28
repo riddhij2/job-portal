b57ef232792a4a26ba132871501aa8196af24678
Riddhi Jha <89968119+riddhij2@users.noreply.github.com>
2024-11-28 10:54:46 +0530

    Update job-application-itoil.component.ts

--- src/app/Pages/home/job-application-itoil/job-application-itoil.component.ts
+++ src/app/Pages/home/job-application-itoil/job-application-itoil.component.ts
@@ -7,8 +7,9 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { District, LanguageLabels, State } from '../../../Models/JobApplication/language-labels';
-import { JobApplicationFormRequest } from '../../../Models/JobApplication/job-application-form-request';
+import { Experience, JobApplicationFormRequest, JobApplicationFormRequestIO, Resume } from '../../../Models/JobApplication/job-application-form-request';
 import { JobApplicationService } from '../../../Services/JobApplication/job-application.service';
+import { switchMap } from 'rxjs';
 
 declare var Swal: any;
 declare var $: any;
@@ -24,25 +25,29 @@ declare var $: any;
 })
 export class JobApplicationITOilComponent {
   jobApplicationForm: FormGroup = new FormGroup({});
-  position: string = 'Software Developer';
+  position: string = '';
   labels = new LanguageLabels();
   states: State[] = [];
   districtsForState: District[] = [];
   districtsForCorrespondenceState: District[] = [];
   language = "";
   MobileNo = '';
-  applicationData: JobApplicationFormRequest = new JobApplicationFormRequest();
+  applicationData: JobApplicationFormRequestIO = new JobApplicationFormRequestIO();
+  resumeModel = new Resume;
+;
   minDate = new Date(2000, 0, 1);
   maxDate = new Date(2020, 0, 1);
 
   constructor(private fb: FormBuilder, private router: Router, private jobappservice: JobApplicationService, private datePipe: DatePipe) { }
 
   ngOnInit(): void {
-    // this.MobileNo = JSON.parse(sessionStorage.getItem('MobileNo') || '')
+    this.MobileNo = JSON.parse(sessionStorage.getItem('MobileNo') || '')
+    this.position = JSON.parse(sessionStorage.getItem('groupName') || '')
     const mobileNo = sessionStorage.getItem('MobileNo');
-          this.MobileNo = mobileNo ? JSON.parse(mobileNo) : '';
+    this.MobileNo = mobileNo ? JSON.parse(mobileNo) : '';
     this.jobApplicationForm = this.fb.group({
       name: ['', Validators.required],
+      KeySkills: ['', Validators.required],
       dateOfBirth: ['', Validators.required],
       gender: ['', Validators.required],
       maritalStatus: ['', Validators.required],
@@ -51,7 +56,7 @@ export class JobApplicationITOilComponent {
       permanentAddress: ['', Validators.required],
       stateId: ['', Validators.required],
       districtId: [{ value: '' }, Validators.required],
-      PinCode: ['', Validators.required, Validators.pattern(/^\d{6}$/)],
+      PinCode: ['', Validators.required],
       resumeFile: ['', Validators.required],
       experiences: this.fb.array([this.createExperienceTab()])
     });
@@ -84,17 +89,19 @@ export class JobApplicationITOilComponent {
     }
   }
   get f() {
-    
+
     return this.jobApplicationForm.controls;
   }
 
   onSubmit(): void {
+    debugger;
     if (this.jobApplicationForm.valid) {
       this.jobApplicationForm.get('mobileNo')?.enable();
-     
       this.applicationData.name = this.jobApplicationForm.controls['name'].value;
       const dateOfBirth = this.jobApplicationForm.controls['dateOfBirth'].value;
-      this.applicationData.dateOfBirth = this.datePipe.transform(dateOfBirth, 'dd/MM/yyyy') || '';
+      const [day, month, year] = dateOfBirth.split('/');
+      const formattedDate = new Date(`${year}-${month}-${day}`);
+      this.applicationData.dateOfBirth = this.datePipe.transform(formattedDate, 'dd/MM/yyyy') || '';
       this.applicationData.gender = this.jobApplicationForm.controls['gender'].value;
       this.applicationData.maritalStatus = this.jobApplicationForm.controls['maritalStatus'].value;
       this.applicationData.emailAddress = this.jobApplicationForm.controls['emailAddress'].value;
@@ -103,228 +110,262 @@ export class JobApplicationITOilComponent {
       this.applicationData.stateId = this.jobApplicationForm.controls['stateId'].value;
       this.applicationData.districtId = this.jobApplicationForm.controls['districtId'].value;
       this.applicationData.PinCode = this.jobApplicationForm.controls['PinCode'].value;
-      this.jobappservice.submitApplication(this.applicationData).subscribe(
-        (result: any) => {
+      this.applicationData.KeySkills = this.jobApplicationForm.controls['KeySkills'].value;
+      const experiences: Experience[] = this.jobApplicationForm.value.experiences.map((exp: any) => ({
+        companyName: exp.company,
+        joiningDate: exp.fromDate,
+        relievingDate: exp.toDate,
+        hodName: exp.hodName,
+        mobileNo: exp.hodmobileNo,
+        emailAddress: exp.hodemailAddress,
+        position: exp.position
+      }));
+      this.applicationData.ExpDtl = experiences;
+      this.resumeModel.mobileNo = this.applicationData.mobileNo;
+      this.jobappservice.submitApplicationIO(this.applicationData).pipe(
+        switchMap((result: any) => {
           if (result.status == 200) {
-            this.router.navigate([''])
+            return this.jobappservice.SubmitResume(this.resumeModel);
+          } else {
+            throw new Error('Failed to submit application');
           }
+        })
+      ).subscribe(
+        (response: any) => {
+          Swal.fire({
+            title: 'Success!',
+            text: 'Your application has been submitted successfully.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          }).then(() => {
+            this.router.navigate(['']);
+          });
         },
         (error: any) => {
           Swal.fire({
-            text: error.error.title,
-            icon: "error"
+            text: error.error?.title || 'An error occurred',
+            icon: 'error'
           });
-        });
+        }
+      );
     }
   }
 
- 
+
   onFileChange(event: Event, fieldName: string): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       const file = input.files[0];
-      const allowedTypes = ['pdf', 'jpg', 'jpeg', 'png'];
+      const allowedTypes = ['pdf'];
       const filename = file.name;
       const extension = filename.split('.').pop()?.toLowerCase();
       if (extension && !allowedTypes.includes(extension)) {
         Swal.fire({
           icon: 'error',
           title: 'Invalid file type!',
-          text: 'Please upload a PDF, JPG, JPEG, or PNG file.',
+          text: 'Please upload a PDF file.',
           confirmButtonText: 'OK'
         });
         input.value = '';
       }
       else {
 
-        this.applicationData.resumeFile = file;
+        this.resumeModel.resumeFile = file;
 
       }
     }
   }
-onLanguageChange(): void {
-  this.changeLabels(this.language ? 'hn' : 'en');
-}
-changeLabels(language: string): void {
-  if(language == 'en') {
-  this.labels = {
-    labelDivision: "Sub Division",
-    labelName: "Name",
-    labelFatherName: "Father's Name",
-    labelDOB: "Date of Birth",
-    labelGender: "Gender",
-    labelMaritalStatus: "Marital Status",
-    labelEmail: "Email Address",
-    labelMobileNo: "Mobile No",
-    labelPermanentAddress: "Permanent Address",
-    labelPincode: "Pincode",
-    labelState: "State",
-    labelDistrict: "District",
-    labelCorrespondenceAddress: "Correspondence Address",
-    labelCPinCode: "Pincode",
-    labelCStateId: "State",
-    labelCDistrict: "District",
-    labelcopyAddress: "Copy Permanent Address to Correspondence Address",
-    labelFatherMobileNo: "Father's Mobile No",
-    labelUANNo: "UAN Number",
-    labelESINo: "ESI Number",
-    labelPanNo: "PAN Number",
-    labelBankName: "Bank Name",
-    labelBankAccountNo: "Bank Account No",
-    labelIFSCCode: "IFSC Code",
-    labelAdharNo: "Aadhaar No",
-    labelFacePhoto: "Upload Face Photo",
-    labelResume: "Resume",
-    labelBankStatement: "Upload Bank Statement / Passbook",
-    labelQualification: "Qualification",
-    labelPan: "Pan Card",
-    labelAdhar: "Aadhar Card",
-    labelMRID: "MRID",
-    labelHodName: "HOD Name",
-    labelHodEmail: "HOD Email Address",
-    labelHodMobileNo: "HOD Mobile No",
-    labelFromDate: "Starting Date",
-    labelToDate: "Leaving Date",
-    labelCompany: "Company Name",
-    labelPosition: "Position",
-    labelExperience: "Experience Details",
-  };
-} else if (language == 'hn') {
-  this.labels = {
-    labelDivision: "उप-विभाग",
-    labelName: "नाम",
-    labelFatherName: "पिता का नाम",
-    labelDOB: "जन्म तिथि",
-    labelGender: "लिंग",
-    labelMaritalStatus: "वैवाहिक स्थिति",
-    labelEmail: "ईमेल पता",
-    labelMobileNo: "मोबाइल नंबर",
-    labelPermanentAddress: "स्थायी पता",
-    labelPincode: "पिनकोड",
-    labelState: "राज्य",
-    labelDistrict: "जिला",
-    labelCorrespondenceAddress: "पत्राचार का पता",
-    labelCPinCode: "पिनकोड",
-    labelCStateId: "राज्य",
-    labelCDistrict: "जिला",
-    labelcopyAddress: "स्थायी पते को पत्राचार पते पर कॉपी करें",
-    labelFatherMobileNo: "पिता का मोबाइल नंबर",
-    labelUANNo: "UAN नंबर",
-    labelESINo: "ESI नंबर",
-    labelPanNo: "पैन नंबर",
-    labelBankName: "बैंक का नाम",
-    labelBankAccountNo: "बैंक खाता नंबर",
-    labelIFSCCode: "IFSC कोड",
-    labelAdharNo: "आधार नंबर",
-    labelFacePhoto: "चेहरे का फोटो अपलोड करें",
-    labelResume: "संक्षिप्त विवरण",
-    labelBankStatement: "बैंक स्टेटमेंट/पासबुक अपलोड करें",
-    labelQualification: "योग्यता",
-    labelPan: "पैन कार्ड",
-    labelAdhar: "आधार कार्ड",
-    labelMRID: "MRID",
-    labelHodName: "HOD नाम",
-    labelHodEmail: "HOD ईमेल पता",
-    labelHodMobileNo: "HOD मोबाइल नंबर",
-    labelFromDate: "आरंभ तिथि",
-    labelToDate: "छोड़ने की तारीख",
-    labelCompany: "कंपनी नाम",
-    labelPosition: "पद",
-    labelExperience: "अनुभव विवरण",
-  };
-}
+  onLanguageChange(): void {
+    this.changeLabels(this.language ? 'hn' : 'en');
   }
-GetState() {
-  this.jobappservice.GetState(1).subscribe(
-    (result: any) => {
-      if (result != null) {
-        this.states = result.body;
-      }
-    },
-    (error: any) => {
-      Swal.fire({
-        text: error.message,
-        icon: "error"
-      });
-    });
-}
-GetCity(stateId: number, type: string) {
-  this.jobappservice.GetCity(stateId).subscribe(
-    (result: any) => {
-      if (result != null) {
-        if (type === 'permanent') {
-          this.districtsForState = result.body;
-        } else if (type === 'correspondence') {
-          this.districtsForCorrespondenceState = result.body;
+  changeLabels(language: string): void {
+    if (language == 'en') {
+      this.labels = {
+        labelDivision: "Sub Division",
+        labelName: "Name",
+        labelFatherName: "Father's Name",
+        labelDOB: "Date of Birth",
+        labelGender: "Gender",
+        labelMaritalStatus: "Marital Status",
+        labelEmail: "Email Address",
+        labelMobileNo: "Mobile No",
+        labelPermanentAddress: "Permanent Address",
+        labelPincode: "Pincode",
+        labelState: "State",
+        labelDistrict: "District",
+        labelCorrespondenceAddress: "Correspondence Address",
+        labelCPinCode: "Pincode",
+        labelCStateId: "State",
+        labelCDistrict: "District",
+        labelcopyAddress: "Copy Permanent Address to Correspondence Address",
+        labelFatherMobileNo: "Father's Mobile No",
+        labelUANNo: "UAN Number",
+        labelESINo: "ESI Number",
+        labelPanNo: "PAN Number",
+        labelBankName: "Bank Name",
+        labelBankAccountNo: "Bank Account No",
+        labelIFSCCode: "IFSC Code",
+        labelAdharNo: "Aadhaar No",
+        labelFacePhoto: "Upload Face Photo",
+        labelResume: "Resume",
+        labelBankStatement: "Upload Bank Statement / Passbook",
+        labelQualification: "Qualification",
+        labelPan: "Pan Card",
+        labelAdhar: "Aadhar Card",
+        labelMRID: "MRID",
+        labelHodName: "HOD Name",
+        labelHodEmail: "HOD Email Address",
+        labelHodMobileNo: "HOD Mobile No",
+        labelFromDate: "Starting Date",
+        labelToDate: "Leaving Date",
+        labelCompany: "Company Name",
+        labelPosition: "Position",
+        labelExperience: "Experience Details",
+        labelKeySkills: "KeySkills",
+      };
+    } else if (language == 'hn') {
+      this.labels = {
+        labelDivision: "उप-विभाग",
+        labelName: "नाम",
+        labelFatherName: "पिता का नाम",
+        labelDOB: "जन्म तिथि",
+        labelGender: "लिंग",
+        labelMaritalStatus: "वैवाहिक स्थिति",
+        labelEmail: "ईमेल पता",
+        labelMobileNo: "मोबाइल नंबर",
+        labelPermanentAddress: "स्थायी पता",
+        labelPincode: "पिनकोड",
+        labelState: "राज्य",
+        labelDistrict: "जिला",
+        labelCorrespondenceAddress: "पत्राचार का पता",
+        labelCPinCode: "पिनकोड",
+        labelCStateId: "राज्य",
+        labelCDistrict: "जिला",
+        labelcopyAddress: "स्थायी पते को पत्राचार पते पर कॉपी करें",
+        labelFatherMobileNo: "पिता का मोबाइल नंबर",
+        labelUANNo: "UAN नंबर",
+        labelESINo: "ESI नंबर",
+        labelPanNo: "पैन नंबर",
+        labelBankName: "बैंक का नाम",
+        labelBankAccountNo: "बैंक खाता नंबर",
+        labelIFSCCode: "IFSC कोड",
+        labelAdharNo: "आधार नंबर",
+        labelFacePhoto: "चेहरे का फोटो अपलोड करें",
+        labelResume: "संक्षिप्त विवरण",
+        labelBankStatement: "बैंक स्टेटमेंट/पासबुक अपलोड करें",
+        labelQualification: "योग्यता",
+        labelPan: "पैन कार्ड",
+        labelAdhar: "आधार कार्ड",
+        labelMRID: "MRID",
+        labelHodName: "HOD नाम",
+        labelHodEmail: "HOD ईमेल पता",
+        labelHodMobileNo: "HOD मोबाइल नंबर",
+        labelFromDate: "आरंभ तिथि",
+        labelToDate: "छोड़ने की तारीख",
+        labelCompany: "कंपनी नाम",
+        labelPosition: "पद",
+        labelExperience: "अनुभव विवरण",
+        labelKeySkills: "प्रमुख कौशल",
+      };
+    }
+  }
+  GetState() {
+    this.jobappservice.GetState(1).subscribe(
+      (result: any) => {
+        if (result != null) {
+          this.states = result.body;
         }
-      }
-    },
-    (error: any) => {
-      Swal.fire({
-        text: error.message,
-        icon: "error"
+      },
+      (error: any) => {
+        Swal.fire({
+          text: error.message,
+          icon: "error"
+        });
       });
-    }
-  );
-}
-get experiences(): FormArray {
-  return this.jobApplicationForm.get('experiences') as FormArray;
-}
-
- // Create a new experience tab
- createExperienceTab(): FormGroup {
-  return this.fb.group({
-    company: ['', Validators.required],
-    position: ['', Validators.required],
-    fromDate: ['', Validators.required],
-    toDate: ['', Validators.required],
-    hodName: ['', Validators.required],
-    hodmobileNo: ['', Validators.required],
-    hodemailAddress: ['', Validators.required],
-  });
-}
-
-addTab(index: number): void {
-  this.experiences.push(this.createExperienceTab());
-}
-
-removeTab(index: number): void {
-  if (this.experiences.length > 1) {
-    this.experiences.removeAt(index);
   }
-}
-// saveExperience(index: number) {
-//   const experience = this.experiences.at(index).value;
-//   console.log('Saved experience:', experience);
-// }
+  GetCity(stateId: number, type: string) {
+    this.jobappservice.GetCity(stateId).subscribe(
+      (result: any) => {
+        if (result != null) {
+          if (type === 'permanent') {
+            this.districtsForState = result.body;
+          } else if (type === 'correspondence') {
+            this.districtsForCorrespondenceState = result.body;
+          }
+        }
+      },
+      (error: any) => {
+        Swal.fire({
+          text: error.message,
+          icon: "error"
+        });
+      }
+    );
+  }
+  get experiences(): FormArray {
+    return this.jobApplicationForm.get('experiences') as FormArray;
+  }
 
-ngAfterViewInit(): void {
-  $('#datepicker').datepicker({
-    format: 'dd/mm/yyyy',
-    autoclose: true,
-    todayHighlight: true,
-  })
-    .on('changeDate', (e: any) => {
-      const selectedDate = e.format('dd/mm/yyyy');
-      this.jobApplicationForm.patchValue({ dateOfBirth: selectedDate });
+  // Create a new experience tab
+  createExperienceTab(): FormGroup {
+    return this.fb.group({
+      company: ['', Validators.required],
+      position: ['', Validators.required],
+      fromDate: ['', Validators.required],
+      toDate: ['', Validators.required],
+      hodName: [''],
+      hodmobileNo: [''],
+      hodemailAddress: ['']
     });
-  $('#datepicker1').datepicker({
-    format: 'dd/mm/yyyy',
-    autoclose: true,
-    todayHighlight: true,
-  })
-    .on('changeDate', (e: any) => {
-      const selectedFromDate = e.format('dd/mm/yyyy');
-      this.jobApplicationForm.patchValue({ fromDate: selectedFromDate });
-    });
-  $('#datepicker2').datepicker({
-    format: 'dd/mm/yyyy',
-    autoclose: true,
-    todayHighlight: true,
-  })
-    .on('changeDate', (e: any) => {
-      const selectedToDate = e.format('dd/mm/yyyy');
-      this.jobApplicationForm.patchValue({ toDate: selectedToDate });
-    });
-}
+  }
+  addTab(index: number): void {
+    this.experiences.push(this.createExperienceTab());
+    this.initializeDatepickers();
+  }
+  removeTab(index: number): void {
+    if (this.experiences.length > 1) {
+      this.experiences.removeAt(index);
+      this.initializeDatepickers();
+    }
+  }
+  saveExperience(index: number) {
+    const experience = this.experiences.at(index).value;
+    console.log('Saved experience:', experience);
+  }
+
+  ngAfterViewInit(): void {
+    $('#datepicker').datepicker({
+      format: 'dd/mm/yyyy',
+      autoclose: true,
+      todayHighlight: true,
+    })
+      .on('changeDate', (e: any) => {
+        const selectedDate = e.format('dd/mm/yyyy');
+        this.jobApplicationForm.patchValue({ dateOfBirth: selectedDate });
+      });
+    this.initializeDatepickers();
+  }
+  initializeDatepickers() {
+    setTimeout(() => {
+      this.experiences.controls.forEach((experience, index) => {
+        $(`#datepicker1-${index}`).datepicker({
+          format: 'dd/mm/yyyy',
+          autoclose: true,
+          todayHighlight: true,
+        }).on('changeDate', (e: any) => {
+          const selectedFromDate = e.format('yyyy-mm-dd');
+          experience.patchValue({ fromDate: selectedFromDate });
+        });
+
+        $(`#datepicker2-${index}`).datepicker({
+          format: 'dd/mm/yyyy',
+          autoclose: true,
+          todayHighlight: true,
+        }).on('changeDate', (e: any) => {
+          const selectedToDate = e.format('yyyy-mm-dd');
+          experience.patchValue({ toDate: selectedToDate });
+        });
+      });
+    }, 0);
+  }
 }

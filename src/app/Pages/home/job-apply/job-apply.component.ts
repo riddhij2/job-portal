117252f85a9538481de6b671e6ad15ba3b254844
Riddhi Jha <89968119+riddhij2@users.noreply.github.com>
2024-11-28 10:58:41 +0530

    Update job-apply.component.ts

--- src/app/Pages/home/job-apply/job-apply.component.ts
+++ src/app/Pages/home/job-apply/job-apply.component.ts
@@ -1,8 +1,9 @@
 import { JsonPipe, KeyValuePipe, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { DesignationItem, LocationItem } from '../../../Models/JobApplication/language-labels';
+import { JobPosting } from '../../../Models/JobPosting/job-posting';
 import { JobApplyService } from '../../../Services/JobApply/job-apply.service';
 declare var Swal: any;
 
@@ -17,10 +18,12 @@ declare var Swal: any;
 export class JobApplyComponent {
   groupId = 0;
   designationShow = false;
+  ITOilShow = false;
   GroupDivisionList: any;
   DesignationList: DesignationItem[] = [];
   LocationList: LocationItem[] = [];
-  constructor(private fb: FormBuilder, private jobapplyservice: JobApplyService, private router: Router) {
+  jobPostings: JobPosting[] = [];
+  constructor(private fb: FormBuilder, private jobapplyservice: JobApplyService, private router: Router, private cdr: ChangeDetectorRef) {
     sessionStorage.clear();
     this.GetGroupdivisions();
   }
@@ -40,21 +43,28 @@ export class JobApplyComponent {
       });
   }
 
-  showOptions(groupId: number) {
-    debugger;
+  showOptions(groupId: number, groupname: string) {
     this.groupId = groupId;
-    this.GetDesignation(this.groupId);
-    this.GetLocation(this.groupId);
-    this.designationShow = true;
+    if (groupId == 3 || groupId == 4) {
+      this.GetDesignation(this.groupId);
+      this.GetLocation(this.groupId);
+      this.ITOilShow = false;
+      this.designationShow = true;
+    }
+    else {
+      this.OpenVacancies(this.groupId);
+      this.designationShow = false;
+      this.ITOilShow = true;
+    }
   }
   GetLocation(groupId: number) {
-    debugger;
     this.jobapplyservice.GetLocation(groupId).subscribe(
       (result: any) => {
-        this.LocationList = result;
+        this.LocationList = result || []; 
         this.DesignationList.forEach((designation: DesignationItem) => {
           designation.selectedLocation = this.LocationList.length > 0 ? this.LocationList[0].locationId : 0;
         });
+        this.cdr.detectChanges();
       },
       (error: any) => {
         Swal.fire({
@@ -75,15 +85,35 @@ export class JobApplyComponent {
         });
       });
   }
+  OpenVacancies(groupId: number) {
+    this.jobapplyservice.OpenVacancies(groupId).subscribe(
+      (result: any) => {
+        this.jobPostings = result;
+      },
+      (error: any) => {
+        Swal.fire({
+          text: error.message,
+          icon: "error"
+        });
+      });
+  }
   redirectToSendOTP(designationId: number, groupName: string, selectedLocationId: number) {
+    debugger;
     if (!selectedLocationId || selectedLocationId === 0) {
       return;
     }
-    debugger;
     sessionStorage.setItem('groupId', JSON.stringify(this.groupId));
     sessionStorage.setItem('designationId', JSON.stringify(designationId));
     sessionStorage.setItem('groupName', JSON.stringify(groupName));
     sessionStorage.setItem('zoneId', JSON.stringify(selectedLocationId));
+    this.router.navigate(['/SendOTP']);
+  }
+  redirectToSendOTPIO(designationId: number, groupName: string) {
+    debugger;
+    sessionStorage.setItem('groupId', JSON.stringify(this.groupId));
+    sessionStorage.setItem('designationId', JSON.stringify(designationId));
+    sessionStorage.setItem('groupName', JSON.stringify(groupName));
+    sessionStorage.setItem('zoneId', JSON.stringify(0));
     this.router.navigate(['/SendOTP']);
   }
 }
